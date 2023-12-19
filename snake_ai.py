6044b85f19692e960e9191e19d970d705857116d
Adil Shaik <adil.shaik@gmail.com>
2023-12-19 12:52:18 -0500

    Added agent file

--- snake_ai.py
+++ snake_ai.py
@@ -131,40 +131,26 @@ class SnakeGameAI:
         idx = clock_wise.index(self.direction)
 
         if np.array_equal(action, [1, 0, 0]):
-            next_idx = (idx + 1) & 4 
             new_dir = clock_wise[idx]
         elif np.array_equal(action, [0, 1, 0]):
-            next_idx = (idx +1) % 4
+            next_idx = (idx + 1) % 4
             new_dir = clock_wise[next_idx]
         else:
             next_idx = (idx - 1) % 4
-            new_dir = clockwise[next_dir] 
+            new_dir = clock_wise[next_idx] 
 
         self.direction = new_dir; 
 
 
         x = self.head.x
         y = self.head.y
-        if direction == Direction.RIGHT:
+        if self.direction == Direction.RIGHT:
             x += BLOCK_SIZE
-        elif direction == Direction.LEFT:
+        elif self.direction == Direction.LEFT:
             x -= BLOCK_SIZE
-        elif direction == Direction.DOWN:
+        elif self.direction == Direction.DOWN:
             y += BLOCK_SIZE
-        elif direction == Direction.UP:
+        elif self.direction == Direction.UP:
             y -= BLOCK_SIZE
 
         self.head = Point(x, y)
-
-if __name__ == '__main__':
-    game = SnakeGame()
-
-    # loop
-    while True:
-        game_over, score = game.play_step()
-        if game_over == True:
-            break
-    
-    print('Final Score', score)
-
-    pygame.quit()
